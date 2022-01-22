763af2897782fc4a9021461a0427e00dfebe4a58
sshalem <shabtay.shalem@gmail.com>
2022-01-22 23:39:50 +0200

    update

--- _7_Data_Structure/_4_Queue/PriorityQueueImpl.java
+++ _7_Data_Structure/_4_Queue/PriorityQueueImpl.java
@@ -14,11 +14,28 @@ public class PriorityQueueImpl {
 	private int count;
 
 	public void add(int item) {
-		/*
-		 * iterate the array from End to beginning
+
+		/**
+		 * assume we have the following PriorityQueue [1, 3, 4, 7]. Now we want to
+		 * Insert (2). After we insert the number (2) to the Queue , it's position needs
+		 * array[1] to be at [1, 2, 3, 4, 7] because it's order in a priority manner. SO
+		 * first we SHIFT RIGTH all elements from position array[1]. then I insert the
+		 * value of the to the position array[1].
 		 */
+
 		if (count == items.length)
 			throw new IllegalStateException();
+
+		/*
+		 * Shifting Items to the Right
+		 */
+		int i = shiftItemsToRight(item);
+		items[i + 1] = item;
+		count++;
+
+	}
+
+	private int shiftItemsToRight(int item) {
 		int i;
 		for (i = count - 1; 0 <= i; i--) {
 			if (items[i] > item) {
@@ -27,14 +44,24 @@ public class PriorityQueueImpl {
 				break;
 			}
 		}
-		items[i + 1] = item;
-		count++;
+		return i;
 	}
 
 	public int remove() {
 		if (count == 0)
 			throw new IllegalStateException();
-		return items[--count];
+
+		int returnItem = items[0];
+		count--;
+		shiftItemsToLeft();
+
+		return returnItem;
+	}
+
+	private void shiftItemsToLeft() {
+		for (int i = 0; i < count; i++) {
+			items[i] = items[i + 1];
+		}
 	}
 
 	public boolean isEmpty() {
@@ -43,7 +70,8 @@ public class PriorityQueueImpl {
 
 	@Override
 	public String toString() {
-		return Arrays.toString(items);
+		int[] copyOfArray = Arrays.copyOf(items, count);
+		return Arrays.toString(copyOfArray);
 	}
 
 }
